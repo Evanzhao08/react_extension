da3892f8918575049f0ed9eddcc8468925fc32f3
Evanzhao08 <15309299853@189.cn>
2023-04-25 09:21:17 +0800

    2. lazyLoad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component,Fragment } from 'react'
-import Demo from './components/8_ErrorBoundary/Parent'
+import React, { Component } from 'react'
+import Demo from './components/2_lazyLoad'
 
 export default class App extends Component {
-	render() {
-		return (
-			<Fragment>
-				<Demo/>
-			</Fragment>
-		)
-	}
+  render() {
+    return (
+      <>
+        <Demo />
+      </>
+    )
+  }
 }
